9369245e540583317a8af1c7eb37c78802f9d9fd
Jheeseong <gmltjd6801@gmail.com>
2022-01-10 22:52:27 +0900

    Embedded type

--- src/main/java/jpa/Member.java
+++ src/main/java/jpa/Member.java
@@ -11,6 +11,17 @@ public class Member {
     private Long id;
     @Column(name = "name")
     private String username;
+    @Embedded
+    private Address homeAddress;
+    @Embedded
+    private Period workPeriod;
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "city",column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name = "street",column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name = "zipcode",column = @Column(name = "WORK_ZIPCODE"))
+    })
+    private Address workAddress;
 //    private Integer age;
 //    @Enumerated(EnumType.STRING)
 //    private RoleType roleType;
@@ -38,5 +49,21 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public Period getWorkPeriod() {
+        return workPeriod;
+    }
+
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
     }
 }
